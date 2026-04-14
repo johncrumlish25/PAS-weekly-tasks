ebdd4c9674a95b841eba8b2056f1395543f592e3
johncrumlish25 <johncrumlish16@gmail.com>
2026-04-14 14:20:03 +0100

    Changed some code to produce better graph. Also added references

--- plottask.py
+++ plottask.py
@@ -2,27 +2,33 @@
 # Histogram of a normal distribution of a 1000 values
 # Author: John Crumlish
 
+# References:
+# https://www.w3schools.com/python/matplotlib_histograms.asp
+# https://numpy.org/doc/stable/reference/generated/numpy.arange.html
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Generates Data
-# Note: The task specifies values between 0–10, but a normal distribution
-# (mean=5, std=2) can produce values outside this range.
+# Generate Data
 data = np.random.normal(5, 2, 1000)
 
 # x & y Values
 x = np.arange(0, 11)
 y = x**3
 
-# Plotting
-plt.hist (data, label="Histogram")
-plt.plot(x, y, label="h(x) = x^3")
+# NORMALISE the function so it fits the histogram scale
+y = y / max(y)
 
-# Labels
-plt.title("Histogram & Function")
+# Plotting
+plt.hist(data, bins=30, density=True, alpha=0.6, label="Histogram")
+plt.plot(x, y, label="Scaled h(x) = x^3")
+
+# Labels & Styling
+plt.title("Histogram of Normal Distribution and Scaled h(x) = x^3")
 plt.xlabel("x Values")
-plt.ylabel("y Values")
+plt.ylabel("Density / Scaled Value")
 plt.legend()
+plt.grid()
 
 # Save Image
 plt.savefig("plottask.png")
